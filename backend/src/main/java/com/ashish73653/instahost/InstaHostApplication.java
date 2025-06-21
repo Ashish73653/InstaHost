9f6ea596b5389154825a2a54affa80eea6c88247
Ashish Singh <as4797singh@gmail.com>
2025-06-21 22:37:05 +0530

    Initial Commit

---
+++ backend/src/main/java/com/ashish73653/instahost/InstaHostApplication.java
@@ -0,0 +1,13 @@
+package com.ashish73653.instahost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaHostApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ashish73653/instahost/InstaHostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish73653.instahost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
